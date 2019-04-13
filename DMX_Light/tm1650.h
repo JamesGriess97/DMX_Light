f2df807c2cec05866f051e740bdb0b396bf57172
James Griess <11860216+JamesGriess97@users.noreply.github.com>
2019-04-13 17:12:23 -0500

    added tm1650 library

---
+++ DMX_Light/tm1650.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   tm1650.h
+ * Author: Owner
+ *
+ * Created on March 25, 2019, 6:16 PM
+ */
+
+#ifndef TM1650_H
+#define	TM1650_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void TM1650_setDigit(uint8_t, char, int);
+void TM1650_init();
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TM1650_H */
+
